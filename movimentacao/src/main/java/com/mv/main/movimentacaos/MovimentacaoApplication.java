cb226fc54be6b7cefe6626ca25cfe449600f7bfb
Isaachbt <isaac.silva1478@gmail.com>
2024-03-20 11:56:29 -0300

    Initial commit

---
+++ movimentacao/src/main/java/com/mv/main/movimentacaos/MovimentacaoApplication.java
@@ -0,0 +1,13 @@
+package com.mv.main.movimentacaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentacaoApplication.class, args);
+	}
+
+}
---
+++ movimentacao/src/test/java/com/mv/main/movimentacaos/MovimentacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.main.movimentacaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
